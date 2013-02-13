717b0b040f2a1c25d058a4abafdb4c4a813f0553
Leon Lin <leonxlin@mit.edu>
2013-02-13 12:15:43 -0500

    set up trie; created Game class

--- game.py
+++ game.py
@@ -7,8 +7,24 @@ class Node:
         self.letter = letter
         self.parent = parent
         self.children = {}
+        self.isEnd = False
+    def process_word(self, word):
+        if len(word) == 0:
+            self.isEnd = True
+            return
+        
+        next_letter = word[0]
+        if next_letter not in self.children:
+            self.children[next_letter] = Node(next_letter, self)
+        self.children[next_letter].process_word(word[1:])
+
+# class to manage game
+class Game:
+    def __init__(self):
+        self.root = Node(None, None)
+        for line in open('wordlist.txt').readlines():
+            self.root.process_word(line.strip())
         
 
-words = open('wordlist.txt').readlines()
-print len(words)
-    
+game = Game()
+
