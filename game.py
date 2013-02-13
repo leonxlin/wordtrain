a4358ba02c4115db7632831b9848d795e13e44f0
Leon Lin <leonxlin@mit.edu>
2013-02-13 12:45:18 -0500

    added setup_response_dicts and load_dictionary

--- game.py
+++ game.py
@@ -20,11 +20,56 @@ class Node:
 
 # class to manage game
 class Game:
+
+    
     def __init__(self):
+        print "========== WORDTRAIN =========="
+        print "beep boop beep boop beep beep boop boop"
+        self.load_dictionary()
+        self.setup_response_dicts()
+
+    def start(self):
+        if self.prompt("Would you like to go first? (Y/N) ", self.yn_response_dict):
+            self.human_move()
+        else:
+            self.computer_move()
+
+    def human_move(self):
+        pass
+    def computer_move(self):
+        pass
+
+    def load_dictionary(self):
         self.root = Node(None, None)
+        print "Loading dictionary..."
         for line in open('wordlist.txt').readlines():
             self.root.process_word(line.strip())
+        print "Dictionary loaded."
+
+    # used for prompts to the user
+    # the response_dicts map possible responses to interpretations
+    def setup_response_dicts(self):
+        self.yn_response_dict = {}
+        self.letter_response_dict = {}
+
+        for s in ["Yes","Y","YES","yes","y"]:
+            self.yn_response_dict[s] = True
+        for s in ["No","N","NO","no","n"]:
+            self.yn_response_dict[s] = False
+        for s in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz":
+            self.letter_response_dict[s] = s.lower()
         
 
-game = Game()
+    # prompts user
+    def prompt(self, text, response_dict):
+        
+        while True:
+            response = raw_input(text)
+            if response in response_dict:
+                break
+            else:
+                print "I don't understand what you mean."
+        return response_dict[response]
 
+game = Game()
+game.start()
