a14fe5463ac277adbee9d6e924f406443d684a33
Leon Lin <leonxlin@mit.edu>
2013-02-13 13:09:42 -0500

    added depth to node; working on the moves

--- game.py
+++ game.py
@@ -8,12 +8,17 @@ class Node:
         self.parent = parent
         self.children = {}
         self.isEnd = False
+        
+        self.depth = 0
+        if parent != None:
+            self.depth = parent.depth + 1
+        
     def process_word(self, word):
         if len(word) == 0:
             self.isEnd = True
             return
         
-        next_letter = word[0]
+        next_letter = word[0].upper()
         if next_letter not in self.children:
             self.children[next_letter] = Node(next_letter, self)
         self.children[next_letter].process_word(word[1:])
@@ -21,23 +26,54 @@ class Node:
 # class to manage game
 class Game:
 
+    MAX_OK_WORD = 3
     
     def __init__(self):
         print "========== WORDTRAIN =========="
         print "beep boop beep boop beep beep boop boop"
         self.load_dictionary()
         self.setup_response_dicts()
+        self.print_rules()
+        
+        self.current_node = self.root
+        self.current_letters = ""
 
-    def start(self):
+
+    def start(self):        
+
         if self.prompt("Would you like to go first? (Y/N) ", self.yn_response_dict):
-            self.human_move()
+            self.finish(self.human_move())
         else:
-            self.computer_move()
+            self.finish(self.computer_move())
+
+    def finish(self, human_wins):
+        pass
+        
 
     def human_move(self):
-        pass
+        letter = self.prompt("Type in a letter: " + self.current_letters, self.letter_response_dict)
+
+        if letter not in self.current_node.children:
+            print "Ha! No word begins with " + self.current_letters + letter + "!"
+            return False
+
+        self.current_node = self.current_node
+        return self.computer_move()
+        
     def computer_move(self):
-        pass
+
+
+        
+        #      Type in a letter: 
+        print "OK. My move:      "
+        return False
+
+    def print_rules(self):
+        print "Rules: We will take turns adding letters to the train."
+        print "At any point in time, the letters must be the beginning"
+        print "of some English word. However, the first player to"
+        print "complete a word longer than " + str(Game.MAX_OK_WORD) + " letters loses."
+        
 
     def load_dictionary(self):
         self.root = Node(None, None)
@@ -57,7 +93,7 @@ class Game:
         for s in ["No","N","NO","no","n"]:
             self.yn_response_dict[s] = False
         for s in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz":
-            self.letter_response_dict[s] = s.lower()
+            self.letter_response_dict[s] = s.upper()
         
 
     # prompts user
